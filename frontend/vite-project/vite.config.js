4799bb0c16f101cdbf547ebd2efafd1cf570f7d8
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-08-13 22:37:48 +0530

    vite.config

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000"
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000"
       }
     }
+  },
+  build: {
+    outDir: 'dist', // Ensure this line is present to specify the output directory
   }
 })
